7e8f6ea99450f23095e1e55e0d3cf3c2a35a7ccf
Flavio <flaviosestu@yahoo.fr>
2023-07-23 18:57:01 +0200

    Configuring /invited command

--- src/main/java/fr/flavio0834/invites/CommandInvited.java
+++ src/main/java/fr/flavio0834/invites/CommandInvited.java
@@ -1,5 +1,10 @@
 package fr.flavio0834.invites;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.CommandExecutor;
@@ -7,6 +12,16 @@ import org.bukkit.command.CommandExecutor;
 public class CommandInvited implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        InvitesManager invitesManager = new InvitesManager(Utils.getPlugin());
+        List<String> invitedPlayers = invitesManager.getInvitedPlayers(sender.getName());
+
+        if (invitedPlayers.isEmpty()) {
+            sender.sendMessage(ChatColor.RED + "You have not invited anyone yet.");
+        } else {
+            sender.sendMessage("Number of invited players : " + ChatColor.GREEN + invitedPlayers.size());
+            sender.sendMessage(String.join(", ", invitedPlayers));
+        }
+
         Utils.log("Command /invited has been executed by " + sender.getName());
         return true;
     }
