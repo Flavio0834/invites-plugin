03b7aaab7e66877dcbc4b556a953ddd3f7138e35
Flavio <flaviosestu@yahoo.fr>
2023-07-30 19:28:27 +0200

    /inviterewards : list all permissions instead of the group name

--- src/main/java/fr/flavio0834/invites/commands/InviteRewardsCommand.java
+++ src/main/java/fr/flavio0834/invites/commands/InviteRewardsCommand.java
@@ -5,6 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 import fr.flavio0834.invites.utils.Utils;
+import fr.flavio0834.invites.utils.LuckpermsManager;
 import fr.flavio0834.invites.utils.RewardsManager;
 
 public class InviteRewardsCommand implements CommandExecutor {
@@ -14,11 +15,15 @@ public class InviteRewardsCommand implements CommandExecutor {
             return false;
         }
 
+        LuckpermsManager luckpermsManager = new LuckpermsManager(Utils.getPlugin());
         RewardsManager rewardsManager = new RewardsManager(Utils.getPlugin());
 
         sender.sendMessage("Rewards :");
         for (String number : rewardsManager.getAllGroups().keySet()) {
-            sender.sendMessage(number + " players : " + rewardsManager.getGroup(number));
+            sender.sendMessage(number + " players :");
+            luckpermsManager.getGroupPermissions(rewardsManager.getGroup(number)).forEach(permission -> {
+                sender.sendMessage(" - " + permission);
+            });
         }
 
         Utils.log("Command /inviterewards has been executed by " + sender.getName() + " with args : "
--- src/main/java/fr/flavio0834/invites/utils/LuckpermsManager.java
+++ src/main/java/fr/flavio0834/invites/utils/LuckpermsManager.java
@@ -1,11 +1,18 @@
 package fr.flavio0834.invites.utils;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.units.qual.A;
+
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.group.Group;
+import net.luckperms.api.node.Node;
 
 public class LuckpermsManager {
     LuckPerms api = LuckPermsProvider.get();
@@ -26,6 +33,18 @@ public class LuckpermsManager {
     public boolean doesGroupExist(String group) {
         Group x = api.getGroupManager().getGroup(group);
         return x != null;
+    }
+
+    public List<String> getGroupPermissions(String group) {
+        Group lpgroup = api.getGroupManager().getGroup(group);
+        List<String> permissions = new ArrayList<>();
+        if (lpgroup != null) {
+            Collection<Node> nodes = lpgroup.getNodes();
+            for (Node node : nodes) {
+                permissions.add(node.getKey());
+            }
+        }
+        return permissions;
     }
 
 }
